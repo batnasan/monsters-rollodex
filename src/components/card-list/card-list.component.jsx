d45c6171125c8570ec9005df79e5570035938da4
Batnasan Byambasuren <batnasan06@gmail.com>
2022-08-04 20:43:20 +1000

    create monsters rollodex as classes

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+
+import Card from '../card/card.component';
+import './card-list.styles.css';
+
+class CardList extends Component {
+  render() {
+    const { monsters } = this.props;
+    console.log('card list render');
+
+    return (
+      <div className='card-list'>
+        {monsters.map((monster) => {
+          return <Card monster={monster} key={monster.id} />;
+        })}
+      </div>
+    );
+  }
+}
+
+export default CardList;
